4cb46456172c920859514fc4f3fe689ded14a38c
GencoMattia <gencomattia@hotmail.it>
2024-05-10 17:56:28 +0200

    fixed alignment from the html container using bootstrap classes

--- js/script.js
+++ js/script.js
@@ -6,22 +6,22 @@ for (let i = 1; i <= 100; i++) {
         console.log ("Fizz");
         squareContainer.append (spanEl);
         spanEl.append("Fizz");
-        spanEl.classList.add("three-Style", "bg-success", "col-1", "border", "border-black", "m-1");
+        spanEl.classList.add("three-Style", "bg-success", "col-1", "border", "border-black", "m-2");
     } else if (i % 5 === 0 && i % 3 !== 0) {
         console.log ("Buzz");
         squareContainer.append (spanEl);
         spanEl.append("Buzz");
-        spanEl.classList.add("five-Style", "bg-warning", "col-1", "border", "border-black", "m-1");
+        spanEl.classList.add("five-Style", "bg-warning", "col-1", "border", "border-black", "m-2");
     } else if (i % 3 === 0 && i % 5 === 0 ) {
         console.log ("FizzBuzz")
         squareContainer.append (spanEl);
         spanEl.append("FizzBuzz");
-        spanEl.classList.add("five-Style", "bg-primary", "text-white", "col-1", "border", "border-black", "m-1");
+        spanEl.classList.add("five-Style", "bg-primary", "text-white", "col-1", "border", "border-black", "m-2");
     } else {
         console.log (i)
         squareContainer.append (spanEl);
         spanEl.append(i);
-        spanEl.classList.add("no-Style", "col-1", "border", "border-black", "m-1");
+        spanEl.classList.add("no-Style", "col-1", "border", "border-black", "m-2");
     }
 }
 
