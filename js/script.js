e1f3c32dd197d479123dc98fcc8f1ed9cdc6999a
GencoMattia <gencomattia@hotmail.it>
2024-05-10 16:43:58 +0200

    Added !== to if function inside for in a way that fix the parameters

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ let sum = 0;
 
 for (let i = 1; i <= 100; i++) {
 
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 !== 0) {
         console.log ("Fizz");
-    } else if (i % 5 === 0) {
+    } else if (i % 5 === 0 && i % 3 !== 0) {
         console.log ("Buzz");
     } else if (i % 3 === 0 && i % 5 === 0 ) {
         console.log ("FizzBuzz")
