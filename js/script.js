1de26b84f8c481585cfff1ae826951e2005b2c33
GencoMattia <gencomattia@hotmail.it>
2024-05-10 17:47:13 +0200

    Added bg color to specific elements

--- js/script.js
+++ js/script.js
@@ -6,14 +6,17 @@ for (let i = 1; i <= 100; i++) {
         console.log ("Fizz");
         squareContainer.append (spanEl);
         spanEl.append("Fizz");
+        spanEl.classList.add("three-Style", "bg-success");
     } else if (i % 5 === 0 && i % 3 !== 0) {
         console.log ("Buzz");
         squareContainer.append (spanEl);
         spanEl.append("Buzz");
+        spanEl.classList.add("five-Style", "bg-warning");
     } else if (i % 3 === 0 && i % 5 === 0 ) {
         console.log ("FizzBuzz")
         squareContainer.append (spanEl);
         spanEl.append("FizzBuzz");
+        spanEl.classList.add("five-Style", "bg-primary", "text-white");
     } else {
         console.log (i)
         squareContainer.append (spanEl);
