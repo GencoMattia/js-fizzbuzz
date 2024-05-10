297821ade488e81b942ee14bdb5a0014fd63bcbc
GencoMattia <gencomattia@hotmail.it>
2024-05-10 17:37:03 +0200

    added a rudimental way to create span with text inside the main container showing the results of the function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 for (let i = 1; i <= 100; i++) {
+    const squareContainer = document.querySelector ("div#element-container");
+    let spanEl = document.createElement("span");
 
     if (i % 3 === 0 && i % 5 !== 0) {
         console.log ("Fizz");
+        squareContainer.append (spanEl);
+        spanEl.append("Fizz");
     } else if (i % 5 === 0 && i % 3 !== 0) {
         console.log ("Buzz");
+        squareContainer.append (spanEl);
+        spanEl.append("Buzz");
     } else if (i % 3 === 0 && i % 5 === 0 ) {
         console.log ("FizzBuzz")
+        squareContainer.append (spanEl);
+        spanEl.append("FizzBuzz");
     } else {
         console.log (i)
+        squareContainer.append (spanEl);
+        spanEl.append(i);
     }
 }
+
+
